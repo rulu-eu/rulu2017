28b28b70d9a581cd1acd4c77d8a75c21f7aa13da
dmathieu <42@dmathieu.com>
2017-02-02 18:15:31 +0100

    Automated commit at 2017-02-02 17:15:31 UTC by middleman-deploy 2.0.0-alpha

---
+++ assets/javascripts/application.js
@@ -0,0 +1,12 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-20067061-3', 'auto');
+ga('send', 'pageview');
+items = document.getElementsByClassName('contact');
+for (var i = 0; i < items.length; i++) {
+  items[i].innerHTML = "hello@rulu.eu";
+}
+;
